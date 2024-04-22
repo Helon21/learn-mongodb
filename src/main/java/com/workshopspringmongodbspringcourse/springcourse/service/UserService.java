22511444c51db77cd51f915455fd4e5cab56a168
Helon21 <helonbentes51@hotmail.com>
2024-04-21 22:00:03 -0300

    feat: insert user in database with postman

--- src/main/java/com/workshopspringmongodbspringcourse/springcourse/service/UserService.java
+++ src/main/java/com/workshopspringmongodbspringcourse/springcourse/service/UserService.java
@@ -1,6 +1,7 @@
 package com.workshopspringmongodbspringcourse.springcourse.service;
 
 import com.workshopspringmongodbspringcourse.springcourse.domain.User;
+import com.workshopspringmongodbspringcourse.springcourse.dto.UserDTO;
 import com.workshopspringmongodbspringcourse.springcourse.repository.UserRepository;
 import com.workshopspringmongodbspringcourse.springcourse.service.exceptions.ObjectNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -21,6 +22,14 @@ public class UserService {
 
     public User findById(String id) {
         return userRepository.findById(id).orElseThrow(() -> new ObjectNotFoundException("User not found"));
+    }
+
+    public User insert(User obj) {
+        return userRepository.insert(obj);
+    }
+
+    public User fromDTO(UserDTO objDTO) {
+        return new User(objDTO.getId(), objDTO.getName(), objDTO.getEmail());
     }
 
 }
