67bfb0f6c7acc54da420ae07c117184ed65f1af6
Helon21 <helonbentes51@hotmail.com>
2024-04-21 21:02:08 -0300

    Created DTO class and refactor UserResources

--- src/main/java/com/workshopspringmongodbspringcourse/springcourse/resources/UserResource.java
+++ src/main/java/com/workshopspringmongodbspringcourse/springcourse/resources/UserResource.java
@@ -1,6 +1,7 @@
 package com.workshopspringmongodbspringcourse.springcourse.resources;
 
 import com.workshopspringmongodbspringcourse.springcourse.domain.User;
+import com.workshopspringmongodbspringcourse.springcourse.dto.UserDTO;
 import com.workshopspringmongodbspringcourse.springcourse.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
@@ -9,6 +10,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
+import java.util.stream.Collectors;
 
 @RestController
 @RequestMapping(value = "/users")
@@ -18,9 +20,10 @@ public class UserResource {
     private UserService userService;
 
     @GetMapping("/find-all")
-    public ResponseEntity<List<User>> findAll() {
+    public ResponseEntity<List<UserDTO>> findAll() {
         List<User> list = userService.findAll();
-        return ResponseEntity.ok().body(list);
+        List<UserDTO> listDto = list.stream().map(x -> new UserDTO(x)).collect(Collectors.toList());
+        return ResponseEntity.ok().body(listDto);
     }
 
 }
